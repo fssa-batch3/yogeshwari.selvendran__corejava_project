7b2286164cc1ec67588faf3fb5b2e37d774658d8
yogeshwari-selvendran <116252201+yogeshwari-selvendran@users.noreply.github.com>
2023-07-28 00:36:15 +0530

    day08 and 09

---
+++ src/test/java/day08/practice/TestCount.java
@@ -0,0 +1,43 @@
+package day08.practice;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class TestCount {
+
+	@Test
+	public void testValidCount() {
+
+		 String input = "Ram,Ram,Superman,Spider,hey,hello,Spider";
+	        Map<String, Integer> countMap = HashMapCount1.countRepetation(input);
+
+	        Assertions.assertEquals(2, countMap.get("Ram"));
+	        Assertions.assertEquals(1, countMap.get("Superman"));
+	        Assertions.assertEquals(2, countMap.get("Spider"));
+	        Assertions.assertEquals(1, countMap.get("hey"));
+	        Assertions.assertEquals(1, countMap.get("hello"));
+
+	}
+	
+	
+	
+	
+	@Test
+	public void testInValidCount() {
+
+		try {
+			HashMapCount1.countRepetation(null);
+			Assertions.fail("Validatevcount failed");
+		} catch (IllegalArgumentException ex) {
+			Assertions.assertEquals("A string cannot be null or empty", ex.getMessage());
+		}
+
+	
+
+	}
+}
